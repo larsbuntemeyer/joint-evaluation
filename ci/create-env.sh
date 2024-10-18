028af4a98dc1d4c65b3e49af2a56195922d86807
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-10-18 10:15:25 +0000

    update ci script

--- ci/create-env.sh
+++ ci/create-env.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 source /mnt/CORDEX_CMIP6_tmp/software/miniforge3/etc/profile.d/conda.sh
 env=catalog-update
 conda env remove -y -n $env || true
 conda env create -f code/environment.yaml -n $env
 conda activate $env
 which python
+
+# Exit the script with exit code 0
+exit 0
