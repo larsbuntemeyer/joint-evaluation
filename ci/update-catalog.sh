5cc6ab833b2487e3803e03d0620837b69d169798
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-10-18 10:39:14 +0000

    update scripts

--- ci/update-catalog.sh
+++ ci/update-catalog.sh
@@ -3,8 +3,8 @@
 source /mnt/CORDEX_CMIP6_tmp/software/miniforge3/etc/profile.d/conda.sh
 env=catalog-update
 conda env list
-#conda activate $env
-#python code/catalog/catalog.py
+conda activate $env
+python code/catalog/catalog.py
 
 # Exit the script with exit code 0
 exit 0
