434f39bb7074e49594567b6bdf00f236b0c19450
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-10-18 10:03:49 +0000

    fix ci environment script

--- ci/create-env.sh
+++ ci/create-env.sh
@@ -1,4 +1,6 @@
-env = catalog-update
+source /mnt/CORDEX_CMIP6_tmp/software/miniforge3/etc/profile.d/conda.sh
+env=catalog-update
 conda env remove -y -n $env || true
 conda env create -f code/environment.yaml -n $env
 conda activate $env
+which python
