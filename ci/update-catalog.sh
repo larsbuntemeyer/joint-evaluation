632c55a7c24f603ae2e1cbf075976c958286662c
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-10-18 10:26:44 +0000

    added catalog script

---
+++ ci/update-catalog.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /mnt/CORDEX_CMIP6_tmp/software/miniforge3/etc/profile.d/conda.sh
+env=catalog-update
+conda activate $env
+python code/catalog/catalog.py
+
+# Exit the script with exit code 0
+exit 0
