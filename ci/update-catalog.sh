4538fb199de05e54b2709823e9a8d9844d6cc24b
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-11-17 19:08:35 +0000

    update script

--- ci/update-catalog.sh
+++ ci/update-catalog.sh
@@ -6,5 +6,13 @@ conda env list
 conda activate $env
 python code/catalog/catalog.py
 
+if [[ `git diff --quiet catalog.csv` ]]; then
+  git commit catalog.csv catalog.xlsx -m"catalog update"
+  git push origin main 
+else
+  echo "no change!"
+fi
+
+
 # Exit the script with exit code 0
 exit 0
