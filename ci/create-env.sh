75b539ec5a837e8bcf238b379d31b88b3473f823
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-11-25 17:43:13 +0100

    added juypterbook ci

--- ci/create-env.sh
+++ ci/create-env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+environment=$1
+
 source /mnt/CORDEX_CMIP6_tmp/software/miniforge3/etc/profile.d/conda.sh
 env=catalog-update
 conda env remove -y -n $env || true
-conda env create -f code/environment.yaml -n $env
+conda env create -f $1 -n $env
 conda activate $env
 which python
 #python code/catalog/catalog.py
