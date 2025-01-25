60c9fbfadf8ef639fbc6682e8be9d06ea6d8a03b
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2025-01-25 20:45:50 +0000

    update ci workflow

--- ci/run-eval-book.sh
+++ ci/run-eval-book.sh
@@ -6,3 +6,16 @@ conda env list
 conda activate $env
 
 jupyter nbconvert --to notebook --execute eval-book/eobs.ipynb
+
+# Check if catalog.csv has changed
+git diff --quiet eval-book/eobs.nbconvert.ipynb
+changed=$?
+
+if [ $changed -ne 0 ]; then
+  echo "committing changes!"
+  git add eval-book/eobs.nbconvert.ipynb
+  git commit --author="github-actions[bot] <github-actions[bot]@users.noreply.github.com>" -m "notebook update"
+  git push origin main
+else
+  echo "no change!"
+fi
