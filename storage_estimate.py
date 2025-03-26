66f2fdf98c60645a3d6b9c1c9de42dfd12c353e7
Jesus Fernandez <Jesus.Fernandez@unican.es>
2025-03-26 17:20:32 +0100

    Update dreq from google sheet and ...
    ... some fixes to the storage estimate
    
     * Discard ESD (most of the variables are not available
     * Discard planned simulations as not likely to meet the deadline for
       evaluation

--- storage_estimate.py
+++ storage_estimate.py
@@ -37,15 +37,17 @@ ngridcells["SEA-12"] = ngridcells["SEA-25"] * 4
 plans = (
     pd.read_csv(
         "https://raw.githubusercontent.com/WCRP-CORDEX/simulation-status/refs/heads/main/CMIP6_downscaling_plans.csv",
-        usecols=["domain", "institute", "experiment", "status"],
+        usecols=["domain", "institute", "experiment", "status", "comments"],
     )
     .query('domain == "EUR-12"')
+    .query('status in ["completed", "running"]')
+    .query('~comments.str.contains("#ESD", na=False)')
     .query('experiment.str.contains("eval")')
 )
 
 simulation_count = plans.pivot_table(
     index="domain", columns="experiment", aggfunc="size", fill_value=0
-)  # .drop(columns = ['selected'])
+).drop(columns = ['selected'], errors='ignore')
 ic(simulation_count)
 
 dreq = pd.read_csv(
@@ -59,8 +61,8 @@ for priorities in dreq["priority"].dropna():
 ic(studies)
 dreq["priority"] = dreq["priority"].fillna("")
 
-for study in sorted(list(studies)):
-    dreq_study = dreq.query("priority.str.contains(@study)").copy()
+for study in sorted(list(studies)) + ['ALL-STUDIES']:
+    dreq_study = dreq.query("priority.str.contains(@study)").copy() if study != 'ALL-STUDIES' else dreq.copy()
     dreq_study["priority"] = study
 
     variable_count = dreq_study.pivot_table(
@@ -96,5 +98,5 @@ for study in sorted(list(studies)):
     ic(size_TB.T.sum())
 
     print(
-        f"Total {study:16} study estimated data request size is: {np.nansum(size_TB.values):8.3f} TB"
+        f"Total {study:21} study estimated data request size is: {np.nansum(size_TB.values):8.3f} TB"
     )
