a48670e7142e4215bb47d5117eaa0591115b25ab
Jesus Fernandez <Jesus.Fernandez@unican.es>
2025-04-16 13:43:24 +0200

    Update after meeting on Apr 16th
    During the topic leads meetign we decided to post the variable
    availability to a website. Here the plots go to an md file.
    
    Also, the Consistency study will be using the same variables as the
    ClimateServices on, so their request is collapsed into one. However the
    data request from the ClimateServices has been split into two
    priorities, to track storage cost separately.
    
    The storage_estimate and variable_availabilty scripts have been moved to
    the code/ folder (they should still be executed from the root folder,
    though)

---
+++ code/variable_availability.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sys
+from icecream import ic
+
+def get_studies(dreq):
+    studies = set()
+    for priorities in dreq["priority"].dropna():
+        for priority in priorities.split():
+            studies.add(priority.strip())
+    return(sorted(list(studies)))
+
+def plot_availability(study, dreq, catalog, outname="availability.png"):
+    dreq_study = dreq.query("priority.str.contains(@study)")
+    dreq_study = dreq_study[["out_name", "frequency"]].rename(columns={"out_name": "variable_id"})
+    #
+    #  Plot variable availability as heatmap
+    #
+    data = catalog.merge(dreq_study, on=["variable_id", "frequency"], how="right")
+    # Avoid showing different subdaily frequencies
+    # data['frequency'] = data['frequency'].replace('.hr', 'xhr', regex = True)
+    data.drop_duplicates(inplace=True)
+    # matrix with models as rows and variables as columns
+    matrix = data.pivot_table(
+        index=["mip_era", "source_id"],
+        columns=["frequency", "variable_id"],
+        aggfunc="size",
+        fill_value=0,
+    )
+    matrix = matrix.replace(0, np.nan)
+    ic(matrix)
+    # Plot as heatmap (make sure to show all ticks and labels)
+    plt.figure(figsize=(14, 12))
+    plt.title(f"Variable availability for {study} study")
+    ax = sns.heatmap(
+        matrix,
+        cmap="YlGnBu_r",
+        annot=False,
+        cbar=False,
+        linewidths=1,
+        linecolor="lightgray",
+    )
+    ax.set_xticks(0.5 + np.arange(len(matrix.columns)))
+    xticklabels = [f"{v}\n({f})" for f, v in matrix.columns]
+    xticklabels = (
+        pd.Series(xticklabels)
+        .replace(r"(.*) \(fx\)", r"\1 (fx)   ", regex=True)
+        .replace(r"(.*) \(xhr\)", r"\1 (xhr)  ", regex=True)
+    ).to_list()
+    ax.set_xticklabels(xticklabels)
+    ax.set_xlabel("variable (freq.)")
+    ax.set_yticks(0.5 + np.arange(len(matrix.index)))
+    yticklabels = [f"{s}" for e, s in matrix.index]
+    ax.set_yticklabels(yticklabels, rotation=0)
+    ax.set_ylabel("source_id")
+    ax.set_aspect("equal")
+    plt.savefig(outname, bbox_inches="tight")
+    plt.close()
+
+if __name__ == "__main__":
+    url = "dreq_EUR_joint_evaluation.csv"
+    dreq = pd.read_csv(url)
+    catalog = pd.read_csv(
+        "catalog.csv", usecols=["variable_id", "frequency", "source_id", "mip_era"]
+    )
+    md_lines = ["# Variable Availability Plots\n"]
+    for study in get_studies(dreq):
+        plot_availability(study, dreq, catalog)
+        plot_path = f"plots/variable_availability__{study}.png"
+        plot_availability(study, dreq, catalog, outname = plot_path)
+        md_lines.append(f"## {study}")
+        md_lines.append(f"![{study}]({plot_path})\n")
+
+    with open("variable_availability.md", "w") as md_file:
+        md_file.write("\n".join(md_lines))
+
---
+++ variable_availability.md
@@ -0,0 +1,46 @@
+# Variable Availability Plots
+
+## AtmRivers
+![AtmRivers](plots/variable_availability__AtmRivers.png)
+
+## Circulation
+![Circulation](plots/variable_availability__Circulation.png)
+
+## ClimateClassification
+![ClimateClassification](plots/variable_availability__ClimateClassification.png)
+
+## ClimateServicesPrio1
+![ClimateServicesPrio1](plots/variable_availability__ClimateServicesPrio1.png)
+
+## ClimateServicesPrio2
+![ClimateServicesPrio2](plots/variable_availability__ClimateServicesPrio2.png)
+
+## Droughts
+![Droughts](plots/variable_availability__Droughts.png)
+
+## Elevation
+![Elevation](plots/variable_availability__Elevation.png)
+
+## FWI
+![FWI](plots/variable_availability__FWI.png)
+
+## HeatStress
+![HeatStress](plots/variable_availability__HeatStress.png)
+
+## Multifeature
+![Multifeature](plots/variable_availability__Multifeature.png)
+
+## Overview
+![Overview](plots/variable_availability__Overview.png)
+
+## PrecipExtremes
+![PrecipExtremes](plots/variable_availability__PrecipExtremes.png)
+
+## Snow
+![Snow](plots/variable_availability__Snow.png)
+
+## Trends
+![Trends](plots/variable_availability__Trends.png)
+
+## WaterBudget
+![WaterBudget](plots/variable_availability__WaterBudget.png)
